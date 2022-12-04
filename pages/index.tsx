c07ca7b20cb5c62f50f2f17f2696a1134685be9e
David Loor <davo20019@gmail.com>
2022-12-03 20:21:34 -0500

    remove the any type error

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home({ allPostsData }) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
               <h2 className={utilStyles.headingLg}>Blog</h2>
               <ul className={utilStyles.list}>
-                {allPostsData.map(({ id, date, title }) => (
+                {allPostsData.map(({ id, date, title }: any) => (
                     <li className={utilStyles.listItem} key={id}>
                       {title}
                       <br />
